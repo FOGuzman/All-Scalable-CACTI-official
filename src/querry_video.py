437bfa6191940654acb46c92a04ccf7dd0b99420
FOGuzman <felipe.guzman.v@mail.pucv.cl>
2024-04-03 15:34:07 -0300

    goo

--- src/querry_video.py
+++ src/querry_video.py
@@ -9,7 +9,7 @@ from functions.utils import compute_ssim_psnr
 from functions.utils import dismantleMeas,assemblyMeas
 #from functions.siren_utils import *
 from functions.model_siren import Siren
-from functions.utils import implay,plot, save_mp4
+from functions.utils import implay,plot,save_mp4
 from torch.utils.data import DataLoader
 from functions.videoLoaderSingle import VideoFramesDataset
 import torch.optim as opt
@@ -17,6 +17,8 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import gc
 import os
+import numpy as np
+import cv2
 
 parser = argparse.ArgumentParser(description='Settings, Data agumentation')
 
@@ -25,17 +27,17 @@ parser.add_argument('--siren_video', default="./implicit_results/clip_5.pth", ty
 parser.add_argument('--mask_path', default="./masks/xinyuan_mask.mat", type=str)
 parser.add_argument('--fullmask_path', default="./masks/mask_spix4_nucmas(512,512,8).mat", type=str)
 parser.add_argument('--frames', default=8, type=int)
-parser.add_argument('--spix', default=1, type=int)
-parser.add_argument('--resolution', default=[256,256], type=eval, help='Dataset resolution')
+parser.add_argument('--spix', default=4, type=int)
+parser.add_argument('--resolution', default=[2048,2048], type=eval, help='Dataset resolution')
 parser.add_argument('--crop_size', default=[2048,2048], type=eval, help='Dataset resolution []')
-parser.add_argument('--siren_batch_size', default=15000, type=int)
+parser.add_argument('--siren_batch_size', default=150000, type=int)
 args = parser.parse_args()
 
 
 ## IMplicit fit
-#siren_model = Siren(in_features=3, out_features=1, hidden_features=512, 
-#                  hidden_layers=4, outermost_linear=True)
-#siren_model.cuda()
+siren_model = Siren(in_features=3, out_features=1, hidden_features=512, 
+                  hidden_layers=4, outermost_linear=True)
+siren_model.cuda()
 
 siren_model = torch.load(args.siren_video).cuda()
 # pr
@@ -44,7 +46,10 @@ siren_model = torch.load(args.siren_video).cuda()
 
 batch_size = args.siren_batch_size
 
-# Full 
+# Full
+Qh,Qw,Qf = (args.resolution[0],args.resolution[0],args.frames*args.spix**2)
+
+coords = np.expand_dims(np.nonzero(np.ones((args.resolution[0],args.resolution[0],args.frames*args.spix**2),dtype=np.uint16)),axis=0)
 coords_x2 = torch.unsqueeze(torch.nonzero(torch.ones((args.resolution[0],args.resolution[0],args.frames*args.spix**2))),0).float()
 coords_x2[:,:,0] = (coords_x2[:,:,0]/(args.resolution[0]-1)*2)-1
 coords_x2[:,:,1] = (coords_x2[:,:,1]/(args.resolution[0]-1)*2)-1
@@ -65,5 +70,31 @@ with torch.no_grad():
 
 ten_out = torch.reshape(out_x2[:,:,0],(args.resolution[0],args.resolution[0],args.frames*args.spix**2))
 
-save_mp4(ten_out.cpu(),"clip_5.mp4")
+
+
+
+save_mp4(ten_out,"./implicit_results/clip_5_ir.mp4")
+
 implay(ten_out.cpu().numpy())# 0.00687
+
+
+
+coords_x2 = torch.unsqueeze(torch.nonzero(torch.ones((args.resolution[0],args.resolution[0],200))),0).float()
+coords_x2[:,:,0] = (coords_x2[:,:,0]/(args.resolution[0]-1)*2)-1
+coords_x2[:,:,1] = (coords_x2[:,:,1]/(args.resolution[0]-1)*2)-1
+coords_x2[:,:,2] = (coords_x2[:,:,2]/(args.frames*args.spix**2-1)*2)-1
+
+coords_x2 = torch.tensor_split(coords_x2,coords_x2.shape[1]//batch_size,dim=1)
+out_x2 = None
+with torch.no_grad():
+    for k in tqdm(range(len(coords_x2)),
+                                 desc ="Querrying... ",colour="red",
+                                 total=len(coords_x2),
+                                 ascii=' 123456789═'):
+        pred_img, coords_out = siren_model(coords_x2[k].float().cuda()) # (1, 3, 256, 256)
+        if out_x2 is None:
+            out_x2 = pred_img.cpu()
+        else:
+            out_x2 = torch.cat((out_x2,pred_img.cpu()),dim=1)
+
+ten_out = torch.reshape(out_x2[:,:,0],(args.resolution[0]*2,args.resolution[0]*2,200))
